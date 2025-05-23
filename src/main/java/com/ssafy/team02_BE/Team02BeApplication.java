d728d98fa84737aa45f2cb4a9af64be3011659d0
onpyeong <gsh447114@gmail.com>
2025-05-23 16:02:16 +0900

    chore: spring 프로젝트 생성

---
+++ src/main/java/com/ssafy/team02_BE/Team02BeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.team02_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team02BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team02BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/team02_BE/Team02BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.team02_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team02BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
