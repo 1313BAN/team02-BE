cd4b51c1bbdfde54a96e267ed446dce0fcf1ec04
onpyeong <gsh447114@gmail.com>
2025-05-26 10:23:26 +0900

    chore: allowedOrigins 수정

--- src/main/java/com/ssafy/team02_BE/config/WebConfig.java
+++ src/main/java/com/ssafy/team02_BE/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.team02_BE.config;
 import com.ssafy.team02_BE.auth.resolver.UserIdResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,6 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserIdResolver userIdResolver;
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -25,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-            .allowedOrigins("http://127.0.0.1:9090") // Vue 앱 주소
+            .allowedOrigins(allowedOrigins) // Vue 앱 주소
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("Content-Type", "Authorization", "X-Requested-With") // 보안 강화
             .exposedHeaders("Authorization") // Authorization 헤더 노출
