1eb6cd18dc4440e74f75ca569af38548e5a51eb2
onpyeong <gsh447114@gmail.com>
2025-05-27 12:09:01 +0900

    chore: 유저 정보 조회 시 찜한 매물 추가

--- src/main/java/com/ssafy/team02_BE/user/controller/dto/UserDetailResponseDTO.java
+++ src/main/java/com/ssafy/team02_BE/user/controller/dto/UserDetailResponseDTO.java
@@ -1,10 +1,13 @@
 package com.ssafy.team02_BE.user.controller.dto;
 
+import com.ssafy.team02_BE.apart.domain.ApartInfo;
 import com.ssafy.team02_BE.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -16,15 +19,17 @@ public class UserDetailResponseDTO {
     private String nickname;
     private String name;
     private String phoneNumber;
+    private List<ApartInfo> userLikes;
 
 
-    public static UserDetailResponseDTO of(User user) {
+    public static UserDetailResponseDTO of(User user, List<ApartInfo> userLikes) {
         return UserDetailResponseDTO.builder()
                 .id(user.getId())
                 .email(user.getEmail())
                 .nickname(user.getNickname())
                 .name(user.getName())
                 .phoneNumber(user.getPhoneNumber())
+                .userLikes(userLikes)
                 .build();
     }
 }
